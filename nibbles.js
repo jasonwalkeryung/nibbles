50c2106acbf5abdd3279764a9a064956b10e3225
Jason Walker-Yung <jason.yung@birchbox.com>
2016-03-09 20:53:47 -0500

    Google API tells us what's for lunch: how about Eleven Madison Park?

--- nibbles.js
+++ nibbles.js
@@ -1,72 +1,98 @@
 (function() {
-  var Slack, _, client, config, irc, parseMessage, pizzamath, slack, slack_config;
+  var Mattermost, Slack, _, client, config, https, irc, mattermost, options, parseMessage, pizzamath, slack, slack_config;
 
-  pizzamath = function(people) {
+  https = require('https');
+
+  pizzamath = function(people, callback) {
     var n;
     n = Math.ceil(parseInt(people, 10) * 3 / 8);
-    return 'Pizza for ' + people + ' = ' + n + ' pies';
+    return callback(("Pizza for " + people + " = " + n + " pies.") + (n > 20 ? " (I'm kind of disgusted.)" : ""));
   };
 
-  parseMessage = function(msg) {
-    var qty;
+  parseMessage = function(msg, callback) {
+    var google_places_api_url, place_fetch, qty;
     if (qty = msg.match(/pizza for (\d+)/i)) {
-      return pizzamath(qty[1]);
+      return pizzamath(qty[1], callback);
+    } else if (msg.match(/^what's for lunch/) && (config.googleapis != null)) {
+      google_places_api_url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?key=" + config.googleapis.key + "&location=" + config.googleapis.lat + "," + config.googleapis.long + "&type=" + config.googleapis.types[0] + "&radius=200";
+      console.log(google_places_api_url);
+      return place_fetch = https.get(google_places_api_url, function(res) {
+        var responseString;
+        console.log("Got response: ${res.statusCode}");
+        responseString = "";
+        res.on('data', function(data) {
+          return responseString += data;
+        });
+        return res.on('end', function() {
+          var places, somewhere;
+          places = JSON.parse(responseString);
+          somewhere = _.sample(places.results);
+          return callback("How about " + somewhere.name + "?");
+        });
+      }).on('error', function(e) {
+        console.log("Got error: ${e.message}");
+        return callback(false);
+      });
     } else {
-      return false;
+      return callback(false);
     }
   };
 
   config = require('config');
 
-  irc = require('irc');
+  _ = require('lodash');
 
-  _ = require('underscore');
+  if (config.irc != null) {
+    irc = require('irc');
+    client = new irc.Client(config.irc.host, config.irc.nick, _.omit(config.irc, function(value, opt) {
+      return opt === 'host' || opt === 'nick';
+    }));
+    client.addListener("registered", function(message) {
+      return console.log("Connected to " + config.irc.host);
+    });
+    client.addListener("message", function(from, to, text, message) {
+      var channel;
+      channel = message.args[0];
+      return parseMessage(text, function(response) {
+        if (response) {
+          return client.say(channel, response);
+        }
+      });
+    });
+  }
 
-  Slack = require('slack-client');
+  if ((config.mattermost != null) && config.mattermost.enabled) {
+    options = {};
+    Mattermost = require('node-mattermost');
+    mattermost = new Mattermost(config.mattermost.url, options);
+  }
 
-  slack_config = {
-    slackToken: config.slack.token,
-    autoReconnect: true,
-    autoMark: true
-  };
-
-  slack = new Slack(slack_config.slackToken, slack_config.autoReconnect, slack_config.autoMark);
-
-  slack.on('open', function() {
-    return console.log("Connected to " + slack.team.name + " as @" + slack.self.name);
-  });
-
-  slack.on('message', function(message) {
-    var channel, response;
-    response = parseMessage(message.text);
-    if (response != null) {
-      console.log(response);
-      channel = slack.getChannelGroupOrDMByID(message.channel);
-      return channel.send(response);
-    }
-  });
-
-  slack.on('error', function(err) {
-    return console.error("Error", err);
-  });
-
-  slack.login();
-
-  client = new irc.Client(config.irc.host, config.irc.nick, _.omit(config.irc, function(value, opt) {
-    return opt === 'host' || opt === 'nick';
-  }));
-
-  client.addListener("registered", function(message) {
-    return console.log("Connected to " + config.irc.host);
-  });
-
-  client.addListener("message", function(from, to, text, message) {
-    var channel, response;
-    channel = message.args[0];
-    response = parseMessage(text);
-    if (response) {
-      return client.say(channel, response);
-    }
-  });
+  if ((config.slack != null) && config.slack.enabled) {
+    Slack = require('slack-client');
+    slack_config = {
+      slackToken: config.slack.token,
+      autoReconnect: true,
+      autoMark: true
+    };
+    slack = new Slack(slack_config.slackToken, slack_config.autoReconnect, slack_config.autoMark);
+    slack.on('open', function() {
+      return console.log("Connected to " + slack.team.name + " as @" + slack.self.name);
+    });
+    slack.on('message', function(message) {
+      if (message != null) {
+        return parseMessage(message.text, function(response) {
+          var channel;
+          if (response) {
+            channel = slack.getChannelGroupOrDMByID(message.channel);
+            return channel.send(response);
+          }
+        });
+      }
+    });
+    slack.on('error', function(err) {
+      return console.error("Error", err);
+    });
+    slack.login();
+  }
 
 }).call(this);
--- src/coffee/nibbles.coffee
+++ src/coffee/nibbles.coffee
@@ -1,52 +1,86 @@
-pizzamath = (people) ->
+https = require('https')
+pizzamath = (people, callback) ->
   n = Math.ceil(parseInt(people,10) * 3 / 8)
-  return 'Pizza for '+people+' = '+n+' pies';
+  callback "Pizza for #{people} = #{n} pies." + (if n > 20 then " (I'm kind of disgusted.)" else "");
 
-parseMessage = (msg) ->
+parseMessage = (msg, callback) ->
   if qty = msg.match(/pizza for (\d+)/i)
-    return pizzamath(qty[1])
-  else return false
+    pizzamath(qty[1], callback)
+  else if msg.match(/^what's for lunch/) and config.googleapis?
+    google_places_api_url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?key=#{config.googleapis.key}&location=#{config.googleapis.lat},#{config.googleapis.long}&type=#{config.googleapis.types[0]}&radius=200"
+    # console.log(google_places_api_url)
+    place_fetch = https.get(google_places_api_url, (res) ->
+      console.log("Got response: #{res.statusCode}");
+
+      # consume response body
+      responseString=""
+
+      res.on('data', (data) ->
+        responseString += data;
+      )
+
+      res.on('end', ->
+        places = JSON.parse(responseString);
+        somewhere = _.sample(places.results)
+        callback "How about #{somewhere.name}?";
+      )
+    ).on('error', (e) ->
+      console.log("Got error: #{e.message}");
+      callback false
+    )
+  else callback false
 
 config = require('config')
-irc = require('irc')
-_ = require('underscore')
+_ = require('lodash')
 
-# Slack
-Slack = require 'slack-client'
-slack_config = {
-    slackToken: config.slack.token # Add a bot at https://my.slack.com/services/new/bot and copy the token here.
-    autoReconnect: true # Automatically reconnect after an error response from Slack.
-    autoMark: true # Automatically mark each message as read after it is processed.
-}
 
-# Slack integration
-slack = new Slack(slack_config.slackToken, slack_config.autoReconnect, slack_config.autoMark)
-
-slack.on 'open', ->
-    console.log "Connected to #{slack.team.name} as @#{slack.self.name}"
-
-slack.on 'message', (message) ->
-  if message?
-    response = parseMessage(message.text)
-    if response
-      channel = slack.getChannelGroupOrDMByID(message.channel)
-      channel.send response
-
-slack.on 'error', (err) ->
-    console.error "Error", err
-
-slack.login()
 
 # IRC
-client = new irc.Client(config.irc.host, config.irc.nick, _.omit(config.irc, (value, opt) ->
-  return opt == 'host' || opt == 'nick'
-))
+if config.irc?
+  irc = require('irc')
+  client = new irc.Client(config.irc.host, config.irc.nick, _.omit(config.irc, (value, opt) ->
+    return opt == 'host' || opt == 'nick'
+  ))
 
-# IRC integration
-client.addListener "registered", (message) ->
-  console.log "Connected to " + config.irc.host
+  # IRC integration
+  client.addListener "registered", (message) ->
+    console.log "Connected to " + config.irc.host
 
-client.addListener "message", (from, to, text, message) ->
-  channel = message.args[0]
-  response = parseMessage(text)
-  if response then client.say(channel, response)
+  client.addListener "message", (from, to, text, message) ->
+    channel = message.args[0]
+    parseMessage(text, (response) ->
+      if response then client.say(channel, response)
+    )
+
+# http://chat.team.birchbox.com/hooks/45z3fbozfpyi385o1q6db3g74c
+if config.mattermost? and config.mattermost.enabled
+  options = {}
+  Mattermost = require('node-mattermost');
+  mattermost = new Mattermost(config.mattermost.url,options)
+
+# Slack
+if config.slack? and config.slack.enabled
+  Slack = require 'slack-client'
+  slack_config = {
+      slackToken: config.slack.token # Add a bot at https://my.slack.com/services/new/bot and copy the token here.
+      autoReconnect: true # Automatically reconnect after an error response from Slack.
+      autoMark: true # Automatically mark each message as read after it is processed.
+  }
+
+  # Slack integration
+  slack = new Slack(slack_config.slackToken, slack_config.autoReconnect, slack_config.autoMark)
+
+  slack.on 'open', ->
+      console.log "Connected to #{slack.team.name} as @#{slack.self.name}"
+
+  slack.on 'message', (message) ->
+    if message?
+      parseMessage(message.text, (response) ->
+        if response
+          channel = slack.getChannelGroupOrDMByID(message.channel)
+          channel.send response
+      )
+  slack.on 'error', (err) ->
+      console.error "Error", err
+
+  slack.login()
